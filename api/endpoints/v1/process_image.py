4686e09a42a032f02915fc8bbcaef20a54f3366a
matteo-bianchi-txt <matteo-bianchi@txtgroup.com>
2024-08-02 21:35:45 +0200

    feat: changed default model, refactor

--- api/endpoints/v1/process_image.py
+++ api/endpoints/v1/process_image.py
@@ -10,10 +10,10 @@ logger = logging.getLogger(__name__)
 
 model_service = get_model_service()
 
+
 @router.post("/image-data")
 async def process_image(file: UploadFile = File(...)):
     image_data = await file.read()
     results = model_service.get_image_elements(image_data)
-
     return JSONResponse(content={"data": [obj.dict() for obj in results]})
 
