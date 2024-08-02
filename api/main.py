90fa1ed85f932b48f34f03aa0b83cf3a3bf5922e
matteo-bianchi-txt <matteo-bianchi@txtgroup.com>
2024-08-02 21:17:26 +0200

    bug: cors allow all, update .dockerignore

--- api/main.py
+++ api/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from endpoints import router as api_router
 
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api")
 
 @app.get("/")
