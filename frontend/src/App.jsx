359ebdd17e0c32c7bf2048b7243862d7ab3373c2
Luca Favini <luca.favini.02@gmail.com>
2024-08-03 03:22:24 +0200

    frontend: add filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
   const [size, setSize] = useState();
   const [normalizeSize, setNormalizeSize] = useState(true)
   const [objects, setObjects] = useState();
+  const [filtered, setFiltered] = useState()
+  const [confidence, setConfidence] = useState(0.5)
 
   async function sendFile() {
     if (!file) return;
@@ -27,6 +29,7 @@ function App() {
       if (response.ok) {
         const data = await response.json()
         setObjects(data.data)
+        setFiltered(data.data)
       } else {
         alert("Error sending file");
       }
@@ -51,6 +54,20 @@ function App() {
       mask_points: [data[Object.keys(data).find(key => key.startsWith("Polygon"))]]
     }
     setObjects([...objects, newObject])
+    setFiltered([...filtered, newObject])
+  }
+
+  function updateConfidence(confidence) {
+    setFiltered(objects.filter(obj => obj.confidence >= confidence))
+    setConfidence(confidence)
+  }
+
+  function updateFilters(objectName) {
+    if (filtered.find(obj => obj.object_name == objectName)) {
+      setFiltered(filtered.filter(obj => obj.object_name != objectName))
+    } else {
+      setFiltered([...filtered, ...objects.filter(obj => obj.object_name == objectName)])
+    }
   }
 
   return (
@@ -76,21 +93,50 @@ function App() {
           <div className="my-10 mx-auto flex flex-row justify-center align-center items-center gap-2">
             <button onClick={sendFile} className={buttonStyle}>Analyze image: extract objects</button>
             <button className={buttonStyle}>Import analyzed objects</button>
-            <button onClick={() => setObjects([])} className={buttonStyle}>Manually select objects</button>
+            <button onClick={() => { setObjects([]); setFiltered([])} } className={buttonStyle}>Manually select objects</button>
           </div>
           <ImagePreview image={file.url} />
         </div>
       )}
-
 
       {(file && objects) && (
         <div>
           <div className="my-10 mx-auto flex flex-row justify-center align-center items-center gap-2">
             <button onClick={reset} className={buttonStyle}>Reset</button>
           </div>
+          <div className="w-full flex flex-row">
 
-          <ImageDrawer objects={objects} image={file.url} size={size} onNewPolygon={onNewPolygon} />
+            <div className="w-2/3">
+              <ImageDrawer objects={filtered} image={file.url} size={size} onNewPolygon={onNewPolygon} />
+            </div>
 
+            <div className="w-1/3">
+
+              <h1 className="text-white text-4xl font-bold mb-4 text-center">Filters:</h1>
+              <div>
+                <div className="text-center">
+                  <h2 className="text-white text-2xl font-bold mb-4">Confidence treshold: {confidence}</h2>
+                  <input type="range" min={0.25} max={1} value={confidence} step="0.01" onChange={(e) => updateConfidence(e.target.value)} class="text-center w-1/2 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+                </div>
+              </div>
+
+              <h1 className="text-white text-4xl font-bold mt-6 text-center">Detected objects:</h1>
+              <h1 className="text-white italic uppercase mb-4 text-center">Click to hide/show</h1>
+
+              {objects.reduce((uniqueObjects, obj) => {
+                if (!uniqueObjects.find(o => o.object_name === obj.object_name)) {
+                  uniqueObjects.push(obj);
+                }
+                return uniqueObjects;
+              }, []).map(obj => (
+                <h2 onClick={() => updateFilters(obj.object_name)} className={`text-2xl italic text-white text-center ${filtered.find(obj2 => obj2.object_name == obj.object_name) ? "font-bold" : "line-through"}`}>
+                  {objects.filter(o => o.object_name === obj.object_name).length}x {obj.object_name.toUpperCase()}
+                </h2>
+              ))}
+
+            </div>
+
+          </div>
         </div>
       )}
     </div>
