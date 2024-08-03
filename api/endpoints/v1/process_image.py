f20b6bf157566ae8e938f9f4a9ac5b5dc52488a0
matteo-bianchi-txt <matteo-bianchi@txtgroup.com>
2024-08-03 08:58:04 +0200

    doc: add components in readme, general documentation improvements

--- api/endpoints/v1/process_image.py
+++ api/endpoints/v1/process_image.py
@@ -1,7 +1,6 @@
 import logging
-from typing import Optional
 
-from fastapi import APIRouter, File, UploadFile, Query, Depends
+from fastapi import APIRouter, File, UploadFile, Depends
 from starlette.responses import JSONResponse
 
 from dto.image_process_request import ImageProcessRequest
@@ -12,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 model_service = get_model_service()
 
+
 # Endpoint used to process images
 
 @router.post("/image-data")
@@ -19,6 +19,6 @@ async def process_image(file: UploadFile = File(...), request: ImageProcessReque
     # Get image in raw format
     image_data = await file.read()
 
-    results = model_service.get_image_elements(image_data, model_name=request.yolo_model_name, conf_threshold=request.confidence_threshold)
+    results = model_service.get_image_elements(image_data, model_name=request.yolo_model_name,
+                                               conf_threshold=request.confidence_threshold)
     return JSONResponse(content={"data": [obj.dict() for obj in results]})
-
